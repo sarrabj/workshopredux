d23a9f409c8be150b9da86d5645535548963e72b
sarrabj <sarrabenjdida92@gmail.com>
2023-01-12 10:27:53 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import {connect} from 'react-redux'
+function App(props) {
+  const mapStateToProps = state =>{
+    return {count : state.counterReducer}
+  }
+ 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <p>{props.count}</p>
+       
       </header>
     </div>
   );
+
+ 
 }
 
-export default App;
+export default connect(mapStateToProps)(App);
---
+++ src/Reducers/counterReducer.js
@@ -0,0 +1,7 @@
+const count=0
+
+
+const counterReducer=(state=count,action)=>{
+    return state
+}
+export default counterReducer
